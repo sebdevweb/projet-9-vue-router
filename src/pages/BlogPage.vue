5a4fa368206d6c7f5109c6f53d862abd5a4b7215
sebdevweb <spansini@squad.fr>
2024-10-08 16:12:39 +0200

    [PROJET-9-VUE-ROUTER] New Feature 1 => Add PostCard component & fetching datas

--- src/pages/BlogPage.vue
+++ src/pages/BlogPage.vue
@@ -1,5 +1,36 @@
 <template>
   <h1>Blog</h1>
+  <div v-if="state === 'error'">Impossible de charger les articles</div>
+  <div v-else :aria-busy="state === 'loading'">
+    <div class="post__container">
+      <PostCard v-for="post in posts" :key="post.id" :post="post" />
+    </div>
+  </div>
+  <button>Page suivante</button>
+
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import PostCard from '@/components/PostCard.vue';
+
+const state = ref("loading")
+const posts = ref([])
+
+onMounted(() => {
+  fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+    .then((r) => {
+      if (r.ok) {
+        return r.json();
+      }
+      throw new Error("Impossible de charger les articles depuis le serveur");
+    })
+    .then((data) => {
+      posts.value = data;
+      state.value = "idle";
+    })
+    .catch((e) => {
+      state.value = "error";
+    });
+});
+</script>
